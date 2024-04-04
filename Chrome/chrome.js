44ec62b1d4c04716159d1fb819e5756fe604a39f
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-04-04 12:33:21 +0530

    chrome update

--- Chrome/chrome.js
+++ Chrome/chrome.js
@@ -1,10 +1,10 @@
-let toggle = document.getElementById("toggle")
+let toggle = document.getElementById("toggle"); // Remove the # symbol
 let dark = document.querySelector(".dark")
 let footer = document.querySelector(".footer")
-let searchField = Document.querySelector(".search-feild")
+let searchField = document.querySelector(".search-feild")
 let btn = document.querySelector('.btn')
 let btn1 = document.querySelector('.btn1')
-let img = document.querySelector('img')
+// let img = document.querySelector('img')
 let findOut = document.querySelector(".find-out")
 let languages = document.querySelector('.languages')
 let hrBreak = document.querySelector(".break")
@@ -22,7 +22,7 @@ function darkModeOn() {
     btn1.style.backgroundColor = "rgb(53,52,52)";
     btn.style.color = 'lightgray';
     btn1.style.color = 'lightgray';
-    img.src = 'menu(2).png';
+    // img.src = 'menu(2).png';
     findOut.style.color = "rgb(39,124,223)";
     languages.style.color = "rgb(39,124,223)";
     hrBreak.style.border = '1px solid rgb(40,40,40)';
@@ -42,7 +42,7 @@ function darkModeOff() {
     btn1.style.backgroundColor = "rgb(248,248,248)";
     btn.style.color = 'rgb(88,88,88)';
     btn1.style.color = 'rgb(88,88,88)';
-    img.src = 'menu.png';
+    // img.src = 'menu.png';
     findOut.style.color = "blue";
     languages.style.color = "blue";
     hrBreak.style.border = '1px solid rgb(223,223,223)';
@@ -58,25 +58,27 @@ if(saveDarkMode === "enabled"){
 
 
 //toggle switch on or off
-toggle.addEventListener("click", function () {
+toggle.addEventListener("click", function() {
     if (toggle.checked == true) {
+        console.log("OK")
         darkModeOn();
     } else {
         darkModeOff();
     }
-
-})
+});
 
 //add search functiality
 function search(){
     let input = searchField.value;
-    window.location.href = ""
+    window.location.href = "https://www.google.com/search?q=" + input + "&rlz=1C1CHBD_enIN1035IN1035&oq=" + input + "&gs_lcrp=EgZjaHJvbWUqDQgAEAAY4wIYsQMYgAQyDQgAEAAY4wIYsQMYgAQyCggBEC4YsQMYgAQyDQgCEC4YgwEYsQMYgAQyCggDEAAYsQMYgAQyCggEEAAYsQMYgAQyBwgFEAAYgAQyDQgGEAAYgwEYsQMYgAQyBwgHEAAYgAQyDQgIEAAYgwEYsQMYgAQyEAgJEAAYgwEYsQMYgAQYigXSAQkzMzMzajBqMTWoAgmwAgE&sourceid=chrome&ie=UTF-8"
 }
 
-googleSearch.addEventListener("click",function(){
-    if(searchField.value == ""){
-        console.log("Please type sonmething!!");
-    }else{
-        search();
+searchField.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        if (searchField.value === "") {
+            console.log("Please type something!!");
+        } else {
+            search();
+        }
     }
-})
+});
